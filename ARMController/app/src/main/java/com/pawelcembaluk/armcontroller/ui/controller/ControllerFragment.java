94c650cd49e8a0413115e2be1ef02ab32de9c86a
AvaPL <241574@student.pwr.edu.pl>
2019-09-12 21:31:37 +0200

    Added seekbars, edited dpad. Currently seekbars don't save their state.

--- ARMController/app/src/main/java/com/pawelcembaluk/armcontroller/ui/controller/ControllerFragment.java
+++ ARMController/app/src/main/java/com/pawelcembaluk/armcontroller/ui/controller/ControllerFragment.java
@@ -1,18 +1,23 @@
 package com.pawelcembaluk.armcontroller.ui.controller;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.pawelcembaluk.armcontroller.R;
+
+import java.util.ArrayList;
 
 public class ControllerFragment extends Fragment {
 
