6868e5870d21956e03dc22650644e4f5b9b49002
Paweł Cembaluk <241574@student.pwr.edu.pl>
2019-09-14 17:59:49 +0200

    Seek bars now save their state. Added text elements related to them. Added landscape controller layout. Minor style changes.

--- ARMController/app/src/main/java/com/pawelcembaluk/armcontroller/ui/controller/ControllerFragment.java
+++ ARMController/app/src/main/java/com/pawelcembaluk/armcontroller/ui/controller/ControllerFragment.java
@@ -1,46 +1,104 @@
 package com.pawelcembaluk.armcontroller.ui.controller;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
 import com.pawelcembaluk.armcontroller.R;
 
-import java.util.ArrayList;
-
 public class ControllerFragment extends Fragment {
 
-    private ControllerViewModel controllerViewModel;
+    private static final String SHARED_PREFERENCES_ANGLES = "angles";
+    private static final String KEY_JOINT_VALUE = "joint_value";
+    private static final String KEY_GRAB_VALUE = "grab_value";
+
+    private SharedPreferences angles;
+    private SeekBar[] jointSeekBars;
+    private SeekBar grabSeekBar;
+    private TextView[] jointTextViews;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        controllerViewModel =
-                ViewModelProviders.of(this).get(ControllerViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_controller, container, false);
-        final TextView textView = root.findViewById(R.id.text_controller);
-        controllerViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        return root;
+        Log.d(getClass().getSimpleName(), "Inflating view");
+        return inflater.inflate(R.layout.fragment_controller, container, false);
     }
 
     @Override
-    public void onDestroyView() {
-        super.onDestroyView();
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        Log.d(getClass().getSimpleName(), "View created.");
+        initializeSeekBars();
+        initializeJointTextViews();
+        initializeAnglesSharedPreferences();
+        loadState();
+        setTexts();
+        setListeners();
+    }
+
+    private void initializeSeekBars() {
+        if (getView() == null) return;
+        jointSeekBars = new SeekBar[3];
+        jointSeekBars[0] = getView().findViewById(R.id.seek_bar_joint_0);
+        jointSeekBars[1] = getView().findViewById(R.id.seek_bar_joint_1);
+        jointSeekBars[2] = getView().findViewById(R.id.seek_bar_joint_2);
+        grabSeekBar = getView().findViewById(R.id.seek_bar_grab);
+    }
+
+    private void initializeJointTextViews() {
+        if (getView() == null) return;
+        jointTextViews = new TextView[3];
+        jointTextViews[0] = getView().findViewById(R.id.text_joint_0);
+        jointTextViews[1] = getView().findViewById(R.id.text_joint_1);
+        jointTextViews[2] = getView().findViewById(R.id.text_joint_2);
+    }
+
+    private void initializeAnglesSharedPreferences() {
+        if (getContext() == null) return;
+        this.angles =
+                getContext().getSharedPreferences(SHARED_PREFERENCES_ANGLES, Context.MODE_PRIVATE);
+    }
+
+    private void loadState() {
+        if (angles == null) return;
+        for (int i = 0; i < jointSeekBars.length; ++i) {
+            int jointValue = angles.getInt(KEY_JOINT_VALUE + "_" + i, 0);
+            jointSeekBars[i].setProgress(jointValue);
+        }
+        int grabValue = angles.getInt(KEY_GRAB_VALUE, 0);
+        grabSeekBar.setProgress(grabValue);
+    }
+
+    private void setTexts() {
+        for (int i = 0; i < jointSeekBars.length && i < jointTextViews.length; ++i)
+            jointTextViews[i].setText(String.valueOf(jointSeekBars[i].getProgress()));
+    }
+
+    private void setListeners() {
+        for (int i = 0; i < jointSeekBars.length && i < jointTextViews.length; ++i) {
+            SeekBar.OnSeekBarChangeListener listener =
+                    OnSeekBarChangeListenerFactory.getTextViewListener(jointTextViews[i]);
+            jointSeekBars[i].setOnSeekBarChangeListener(listener);
+        }
+    }
+
+    @Override
+    public void onPause() {
+        if (angles == null) return;
+        SharedPreferences.Editor anglesEditor = angles.edit();
+        for (int i = 0; i < jointSeekBars.length; ++i)
+            anglesEditor.putInt(KEY_JOINT_VALUE + "_" + i, jointSeekBars[i].getProgress());
+        anglesEditor.putInt(KEY_GRAB_VALUE, grabSeekBar.getProgress());
+        anglesEditor.apply();
+        super.onPause();
     }
 }
