73dea4f508212415ddb1b305453c3a06d074190c
Paweł Cembaluk <241574@student.pwr.edu.pl>
2019-09-28 17:18:43 +0200

    Added initial angles, shutdown mechanism and newline in sent commands.

--- Arm.py
+++ Arm.py
@@ -1,54 +1,76 @@
 from adafruit_servokit import ServoKit
 from bluetooth import BluetoothError
-import re
 
 from BufferedData import BufferedData
 
 
 class Arm:
-    def __init__(self, bluetoothConnection, servoNumber, pulseWidths):
+    def __init__(self, bluetoothConnection, servoNumber, pulseWidths, initialAngles):
         self.bluetoothConnection = bluetoothConnection
         self.servoNumber = servoNumber
         self.servoKit = ServoKit(channels=16)
         for index in range(servoNumber):
             servo = self.servoKit.servo[index]
             servo.set_pulse_width_range(pulseWidths[index][0], pulseWidths[index][1])
+            servo.angle = initialAngles[index]
         self.bufferedData = BufferedData()
 
     def readCommands(self):
-        # returns True if the arm received an "exit" command
+        # returns True if the arm received a shutdown command
         # and False if the client disconnected
         try:
-            return self.readUntilExit()
+            return self.readUntilShutdown()
         except BluetoothError:
             print("Disconnected")
             return False
 
-    def readUntilExit(self):
+    def readUntilShutdown(self):
         while True:
             data = self.bluetoothConnection.readData(1024)
             self.bufferedData.append(data)
             commands = self.bufferedData.flush()
             for command in commands:
-                if command == "exit":
+                print(command)
+                if command == "shutdown":
                     return True
                 self.processCommand(command)
 
     def processCommand(self, command):
-        print(command)
         splitCommand = command.split(' ')
-        if splitCommand[0] == "angle":
-            servoIndex = int(splitCommand[1])
-            self.changeAngle(servoIndex, int(splitCommand[2]))
-        elif splitCommand[0] == "angles":
-            for index in range(self.servoNumber - 1):
-                self.bluetoothConnection.writeData(
-                    "angle " + str(index) + " " + str(round(self.servoKit.servo[index].angle)) + '\n')
-            self.bluetoothConnection.writeData(
-                "angle grab " + str(round(self.servoKit.servo[self.servoNumber - 1].angle)) + '\n')
+        if command == "angles":
+            self.sendAngles()
+        elif self.isNavigationCommand(command):
+            pass # TODO: Implement movement.
+        elif splitCommand[0] == "angle":
+            self.changeAngle(splitCommand)
         else:
-            self.bluetoothConnection.writeData("Unknown command\n")
+            self.sendUnknownCommand()
 
-    def changeAngle(self, servoIndex, angle):
+    def sendAngles(self):
+        for index in range(self.servoNumber - 1):
+            self.bluetoothConnection.writeData(self.getJointAngleCommand(index))
+        self.bluetoothConnection.writeData(self.getGrabAngleCommand())
+
+    def isNavigationCommand(self, command):
+        navigationCommands = ["forward", "back", "left", "right"]
+        return command in navigationCommands
+
+    def getJointAngleCommand(self, index):
+        return "angle " + str(index) + " " + self.getAngle(index)
+
+    def getAngle(self, index):
+        angle = self.servoKit.servo[index].angle
+        return str(round(angle))
+
+    def getGrabAngleCommand(self):
+        index = self.servoNumber - 1
+        return "angle grab " + self.getAngle(index)
+
+    def changeAngle(self, splitCommand):
+        servoIndex = int(splitCommand[1])
+        angle = int(splitCommand[2])
         if 0 <= angle <= 180:
             self.servoKit.servo[servoIndex].angle = angle
+
+    def sendUnknownCommand(self):
+        self.bluetoothConnection.writeData("Unknown command")
